a8475537ecfa0175d1674b91472649e8c2e9f204
Srinadhithub <117295296+Srinadhithub@users.noreply.github.com>
2023-05-05 14:12:17 +0530

    Update HotelManagementController.java

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -4,7 +4,6 @@ import com.driver.model.Booking;
 import com.driver.model.Facility;
 import com.driver.model.Hotel;
 import com.driver.model.User;
-import com.driver.services.HotelManagementService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
